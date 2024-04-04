161397cc89e495546083566f9d5c39d1c4a58f15
Pepe Toño <josefuentes.at@gmail.com>
2024-04-04 11:48:36 -0600

    update main and boot

--- webServer_HolaMundo/boot.py
+++ webServer_HolaMundo/boot.py
@@ -1,31 +1,24 @@
-#Importar sockets
 try:
   import usocket as socket
 except:
   import socket
 
-#Permite conectar ell ESP32 a la red WiFi
 import network
 
-#Apaga los mensajes de depuración
 import esp
 esp.osdebug(None)
 
-#Colector de basura para ahorrar espacio en flash
 import gc
 gc.collect()
 
-#Credenciales
-ssid = 'JoseFuentesTomas'
+ssid = 'PepeModem_2.4Gnormal'
 password = 'JoseFuentesTomas'
 
-#Crea la interfaz de estación
 station = network.WLAN(network.STA_IF)
 
 station.active(True)
 station.connect(ssid, password)
 
-#Asegura que funcione solo mientras esta conectado
 while station.isconnected() == False:
   pass
 
