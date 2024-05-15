55872164b18731130377b930b116ebd2e7a0bc39
Pepe Toño <josefuentes.at@gmail.com>
2024-05-14 22:55:39 -0600

    Update main.py

--- OLED/main.py
+++ OLED/main.py
@@ -1,22 +1,31 @@
-from machine import Pin, SoftI2C
-import ssd1306
+from machine import Pin, ADC, SoftI2C, Timer
 from time import sleep
+import ssd1306
 
-# ESP32 Pin assignment 
+sensor = ADC(Pin(4))
+sensor.width(ADC.WIDTH_10BIT)## 10 bits
+sensor.atten(ADC.ATTN_11DB)##Full range voltage 3.3V
+
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-
-# ESP8266 Pin assignment
-#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
-
 oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-oled.text('Hello, World 1!', 0, 0)
-oled.text('Hello, World 2!', 0, 10)
-oled.text('Hello, World 3!', 0, 20)
-oled.invert(False)
-#oled.scroll(10,10)
-#for i in range(10):
-    #oled.scroll(0,3)
-oled.show()
+def timer_interrupt(t):
+    global reading
+    reading = sensor.read()
+
+timer = Timer(-1)
+timer.init(period=500, mode=Timer.PERIODIC, callback=timer_interrupt)
+
+reading = sensor.read()
+
+while True:
+    oled.text('Sensor Value', 0, 0)
+    oled.text(str(reading), 0, 10)
+    oled.show()
+    oled.fill(0)
+    
+
+
+
